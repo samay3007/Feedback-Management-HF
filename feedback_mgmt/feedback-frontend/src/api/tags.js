788cb3f382855146b11c8254fda7c8076763ec5c
HappyFox <happyfox@HF-MacBook.local>
2025-07-30 13:39:07 +0530

    Initial-commit of tag feature had bugs

--- feedback_mgmt/feedback-frontend/src/api/tags.js
+++ feedback_mgmt/feedback-frontend/src/api/tags.js
@@ -1,4 +1,6 @@
-// api/tags.js
+// api/tags.js//bug-fix
+// feedback_mgmt/feedback-frontend/src/api/tags.js
+// This file handles API requests related to tags
 import axiosInstance from './axiosInstance';
 
 export const getTags = async () => {
